2b2ec38b0dc0149d4d972177368fb5fa622593bd
Josh Holtz <me@joshholtz.com>
2026-06-17 21:45:38 -0500

    bump rc to v0.1.0-beta.1

--- Formula/rc.rb
+++ Formula/rc.rb
@@ -1,28 +1,28 @@
 class Rc < Formula
   desc "RevenueCat command line interface"
   homepage "https://github.com/RevenueCat/revenuecat-cli"
-  version "0.1.1"
+  version "0.1.0-beta.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.1/rc_0.1.1_darwin_arm64.tar.gz"
-      sha256 "24e6ebd4f442b241bab692f5e830cc96a6c94e91986b9fdd8f0639c14546e39f"
+      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.0-beta.1/rc_0.1.0-beta.1_darwin_arm64.tar.gz"
+      sha256 "dc1abc2e30fafcf448631b6f025779fe1985d35e8f20397030cb43d2e0e7539b"
     end
     on_intel do
-      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.1/rc_0.1.1_darwin_amd64.tar.gz"
-      sha256 "a30a4a1377430c16559eb1e2080538492ddc0d51c61b81243dfb3c9794118bd0"
+      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.0-beta.1/rc_0.1.0-beta.1_darwin_amd64.tar.gz"
+      sha256 "398b731da9de27616db63cd3fda4daedea05db85ca8b27d4bc12a9b52a684326"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.1/rc_0.1.1_linux_arm64.tar.gz"
-      sha256 "1c4710d75c3eff09d8d312fd0eb405e7f471a1f821186205b63db0977a40983a"
+      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.0-beta.1/rc_0.1.0-beta.1_linux_arm64.tar.gz"
+      sha256 "e663801c963f63251e9ef79fb37fb0e70b457c886e21c180873cbeb7986cd73b"
     end
     on_intel do
-      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.1/rc_0.1.1_linux_amd64.tar.gz"
-      sha256 "659bad65e9fe1c415905275332a31ac40b8949d32dad116decf8ca308b1936e8"
+      url "https://github.com/RevenueCat/revenuecat-cli/releases/download/v0.1.0-beta.1/rc_0.1.0-beta.1_linux_amd64.tar.gz"
+      sha256 "d614eb9d239158045fa66e4c9430f91e570b3cc1fae42fc3efc8af5fb8e9931d"
     end
   end
 
